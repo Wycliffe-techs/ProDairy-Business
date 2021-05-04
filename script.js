38036e01cb27eabff40bcde70706c5a68c49bb21
wyclife omondi <wyliffeomondi01@gmail.com>
2021-05-04 22:35:46 +0300

    script

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-//Calculating daily milk production
+// //Calculating daily milk production
 
-const dailyMilkProduction = function (cows, ltrs) {
-    return cows * ltrs;
-}
+// const dailyMilkProduction = function (cows, ltrs) {
+//     return cows * ltrs;
+// }
 
-const shedADaily = dailyMilkProduction(34, 15);
-const shedBDaily = dailyMilkProduction(11, 28);
-const shedCDaily = dailyMilkProduction(27, 18);
-const shedDDaily = dailyMilkProduction(52, 11);
-
-
-//Calculating daily income
-
-let dailyLtrsTotal = shedADaily +
-    shedBDaily + shedCDaily + shedDDaily;
-
-let dailyIncome = dailyLtrsTotal * 45;
-
-//Calculating daily income
+// const shedADaily = dailyMilkProduction(34, 15);
+// const shedBDaily = dailyMilkProduction(11, 28);
+// const shedCDaily = dailyMilkProduction(27, 18);
+// const shedDDaily = dailyMilkProduction(52, 11);
 
 
-//Independent Variable
-let annualDays = 365;
-//Independent Variable
+// //Calculating daily income
 
-//Milk produced per shed report
+// let dailyLtrsTotal = shedADaily +
+//     shedBDaily + shedCDaily + shedDDaily;
+
+// let dailyIncome = dailyLtrsTotal * 45;
+
+// //Calculating daily income
+
+
+// //Independent Variable
+// let annualDays = 365;
+// //Independent Variable
+
+// //Milk produced per shed report
